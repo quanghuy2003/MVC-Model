f8b0d28f66e6313234eef8fc0ba042294dfe2d25
quanghuy2002 <hoangqunghuy753@gmail.com>
2021-11-29 16:54:35 +0700

    ..

---
+++ src/main/java/CustomerServlet.java
@@ -0,0 +1,45 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "CustomerServlet", value = "/CustomerServlet")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            default:
+                break;
+        }
+    }
+}
